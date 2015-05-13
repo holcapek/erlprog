8ee99f64b4547285335c7c85da653ffbc6f8519d
Jan Holcapek <holcapek@gmail.com>
2015-05-13 02:13:25 +0200

    Exercise 03-03

---
+++ 03-03/sideff.erl
@@ -0,0 +1,26 @@
+% Write a function that prints out the integers between 1 and N.
+% Hint: use io:format("Number:~p~n",[N]).
+
+% Write a function that prints out the even integers between 1 and N.
+% Hint: use guards.
+
+-module(sideff).
+-export([printall/1, printeven/1]).
+
+printall(N, N) ->
+	io:format("Number:~p~n", [ N ]);
+printall(N, I) when I < N ->
+	io:format("Number:~p~n", [ I ]),
+	printall(N, I + 1).
+printall(N) when N > 0 ->
+	printall(N, 1).
+
+printeven(N, I) when I > N ->
+	ok;
+printeven(N, I) when I rem 2 == 0 ->
+	io:format("Number:~p~n", [ I ]),
+	printeven(N, I + 2);
+printeven(N, I) when I rem 2 == 1 ->
+	printeven(N, I + 1).
+printeven(N) when N > 0 ->
+	printeven(N, 2).
