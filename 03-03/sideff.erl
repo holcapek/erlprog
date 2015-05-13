9ec98cbb75f9fc5f14d38325bc7997745c0db900
Jan Holcapek <holcapek@gmail.com>
2015-05-13 02:19:01 +0200

    Exercise 03-03: spaces over tabs

--- 03-03/sideff.erl
+++ 03-03/sideff.erl
@@ -8,19 +8,19 @@
 -export([printall/1, printeven/1]).
 
 printall(N, N) ->
-	io:format("Number:~p~n", [ N ]);
+  io:format("Number:~p~n", [ N ]);
 printall(N, I) when I < N ->
-	io:format("Number:~p~n", [ I ]),
-	printall(N, I + 1).
+  io:format("Number:~p~n", [ I ]),
+  printall(N, I + 1).
 printall(N) when N > 0 ->
-	printall(N, 1).
+  printall(N, 1).
 
 printeven(N, I) when I > N ->
-	ok;
+  ok;
 printeven(N, I) when I rem 2 == 0 ->
-	io:format("Number:~p~n", [ I ]),
-	printeven(N, I + 2);
+  io:format("Number:~p~n", [ I ]),
+  printeven(N, I + 2);
 printeven(N, I) when I rem 2 == 1 ->
-	printeven(N, I + 1).
+  printeven(N, I + 1).
 printeven(N) when N > 0 ->
-	printeven(N, 2).
+  printeven(N, 2).
